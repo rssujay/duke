5d1b43f64005d21778c41efe18255efad5301bb0
rssujay <rssujay97@gmail.com>
2019-08-22 22:50:07 +0800

    Complete Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Duke {
     private static String horizontalLine = "____________________________________________________________";
@@ -16,7 +17,8 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
-        levelOneEcho();
+        //levelOneEcho();
+        levelTwoEcho();
     }
 
     private static void levelOneEcho(){
@@ -33,6 +35,38 @@ public class Duke {
             else{
                 System.out.println(horizontalLine);
                 System.out.println(input);
+                System.out.println(horizontalLine);
+            }
+        }
+    }
+
+    private static void levelTwoEcho(){
+        Scanner scanner = new Scanner(System.in);
+        Vector<String> inputs = new Vector<>();
+
+        while(scanner.hasNext()){
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")){
+                System.out.println(horizontalLine);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
+                break;
+            }
+
+            else if (input.equals("list")){
+                System.out.println(horizontalLine);
+                for (int i = 0; i < inputs.size(); i++){
+                    System.out.print(i+1);
+                    System.out.println(". " + inputs.get(i));
+                }
+                System.out.println(horizontalLine);
+            }
+
+            else{
+                inputs.addElement(input);
+                System.out.println(horizontalLine);
+                System.out.println("added: " + input);
                 System.out.println(horizontalLine);
             }
         }
