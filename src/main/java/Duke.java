31c16ac678d0523cc4fd619773bd739e8a2f6d49
rssujay <rssujay97@gmail.com>
2019-08-22 22:34:26 +0800

    Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String horizontalLine = "____________________________________________________________";
+
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+         */
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        levelOneEcho();
+    }
+
+    private static void levelOneEcho(){
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            String input = scanner.nextLine();
+            if (input.equals("bye")){
+                System.out.println(horizontalLine);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
+                break;
+            }
+
+            else{
+                System.out.println(horizontalLine);
+                System.out.println(input);
+                System.out.println(horizontalLine);
+            }
+        }
     }
 }
